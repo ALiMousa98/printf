517ab7bff5f3c9a633ebb8c44c2818b4cf07f3e9
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-03-27 16:02:54 -0700

    _printf.c -m

---
+++ _printf.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+    va_list args;
+    va_start(args, format);
+
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                {
+                    char c = (char) va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
+                }
+                case 's':
+                {
+                    char *s = va_arg(args, char *);
+                    while (*s != '\0')
+                    {
+                        putchar(*s);
+                        s++;
+                        count++;
+                    }
+                    break;
+                }
+                case '%':
+                {
+                    putchar('%');
+                    count++;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
+        format++;
+}
+
+ 	va_end(args);
+ 	return count;
+}
