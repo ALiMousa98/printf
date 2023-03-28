4c11c9b9f1c8603145c8719f30736f6eb0ddf00d
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-03-28 15:24:19 -0700

    update main.h -a

--- main.h
+++ main.h
@@ -6,6 +6,17 @@
 #include <stdarg.h>
 #include "_putchar.c"
 
+/**
+  * struct specifiers - Struct specifiers
+  * @specifier: The conversion specifier
+  * @f: The function pointer
+  */
+typedef struct specifiers
+{
+	char *specifier;
+	int (*f)(va_list args);
+} spc_dt;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_char(va_list args);
