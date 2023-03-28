e98fa5064bd6bc28ba6868fcaba019838d68427b
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-03-28 14:30:19 -0700

    main.h update -m

--- main.h
+++ main.h
@@ -2,7 +2,17 @@
 #define _MAIN_H_
 
 #include <stdarg.h>
+#include "_putchar.c"
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+int _print_a_char(va_list args);
+int _print_a_string(va_list args);
+int _print_format(const char *format, va_list args);
+int _print_spec(char format, va_list args);
+int _print_invalid_spec(char prev_format, char format, int count);
+int _print_a_integer(va_list args);
+void _recursion_integer(int a);
+int _validate_char(char _type);
 
 #endif
